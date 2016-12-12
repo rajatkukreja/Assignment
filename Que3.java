83f8ab268d4af85506637b547317b49e5d823436
rajat <rajatkukreja28@gmail.com>
2016-12-12 14:35:53 +0530

    Que3 completed

---
+++ Que3.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Que3 {
+    public static void main(String[] ar){
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        for(int i=0; i<n; i++)
+            System.out.println("*");
+    }
+
+}

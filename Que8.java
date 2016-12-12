0c812628e7dea6cf241db9110dd24fd1a2013744
rajat <rajatkukreja28@gmail.com>
2016-12-12 17:36:39 +0530

    Refactored isDivisibleBy3 and isDivisibleBy5 into isDivisible

--- Que8.java
+++ Que8.java
@@ -11,23 +11,15 @@ public class Que8 {
 
     public static void FizzBuzz(int start,int end){
         for(int i=start; i<= end; i++){
-            boolean fizz = isDivisibleBy3(i);
-            boolean buzz = isDivisibleBy5(i);
+            boolean fizz = isDivisible(i,3,"Fizz");
+            boolean buzz = isDivisible(i,5,"Buzz");
             System.out.println(fizz || buzz ? "" : i + "");
         }
     }
 
-    public static boolean isDivisibleBy3(int n){
-        if(n % 3 == 0) {
-            System.out.print("Fizz");
-            return true;
-        }
-        return false;
-    }
-
-    public static boolean isDivisibleBy5(int n){
-        if(n % 5 == 0) {
-            System.out.print("Buzz");
+    public static boolean isDivisible(int number,int divisor, String str){
+        if(number % divisor == 0) {
+            System.out.print(str);
             return true;
         }
         return false;

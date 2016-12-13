207ac3d123ad4857c695703c7862e0301e8ea308
rajat <rajatkukreja28@gmail.com>
2016-12-13 13:06:56 +0530

    Reverted previous changes and converted Integer to int

--- Que9.java
+++ Que9.java
@@ -12,41 +12,23 @@ public class Que9 {
     }
 
     public static int[] generate(int n){
+        if(n == 1)
+            return new int[] {1};
         List primeFactors = new List();
-        getSmallerPrimeFactors(n, primeFactors);
-        getLargerPrimeFactors(n, primeFactors);
-        return primeFactors.toArray();
-    }
-
-    private static void getLargerPrimeFactors(int n, List primeFactors) {
-        if(primeFactors.size() == 0) {
-            primeFactors.add(n);
-            return;
-        }
-        for(int i=primeFactors.size()-1; i>=0; i--){
-            int fact = n/primeFactors.get(i);
-            if(isPrime(fact))
-                primeFactors.add(fact);
-        }
-    }
-
-    private static void getSmallerPrimeFactors(int n, List primeFactors) {
-        for(int i=2; i<=maxPossibleSmallerFactor(n); i++){
+        for(int i=2; i<=n; i++){
             if(n % i == 0 && isPrime(i))
                 primeFactors.add(i);
         }
+        return primeFactors.toArray();
     }
 
     public static boolean isPrime(int n){
-        for(int i=2; i<=maxPossibleSmallerFactor(n); i++){
+        int end = (int)Math.sqrt(n);
+        for(int i=2; i<=end; i++){
             if(n % i == 0)
                 return false;
         }
         return true;
-    }
-
-    public static int maxPossibleSmallerFactor(int n){
-        return (int)Math.sqrt(n);
     }
 }
 
@@ -62,13 +44,5 @@ class List {
         for(int i=0; i<arr.length; i++)
             arr[i] = ((Integer)list.get(i)).intValue();
         return arr;
-    }
-
-    public int size(){
-        return list.size();
-    }
-
-    public int get(int index){
-        return ((Integer)list.get(index)).intValue();
     }
 }
